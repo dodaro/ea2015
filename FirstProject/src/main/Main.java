f9c13057d94dc7d804553a228b4d810eb01eeb9b
arturog2k <arturog2k@gmail.com>
2015-11-01 20:14:10 +0100

    First Commit

---
+++ FirstProject/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD!");
+	}
+
+}
