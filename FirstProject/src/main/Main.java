2515d0b8f134ae6e10c66735e4f77727b77bcad3
arturog2k <arturog2k@gmail.com>
2015-11-01 20:27:01 +0100

    New Changed Stupid Try

--- FirstProject/src/main/Main.java
+++ FirstProject/src/main/Main.java
@@ -1,13 +1,14 @@
 package main;
 
 public class Main {
-
+	
+	//First upload gitHub
 	public Main() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD!");
+		System.out.println("I've been on GitHub");
 	}
 
 }
