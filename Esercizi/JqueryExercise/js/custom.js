07caab930609aca2f0d7cc1630afaac731dd2b52
edge33 <edge33@email.ru>
2015-11-07 13:27:52 +0100

    Finishing the exercise, the layout needs to be revised though

--- Esercizi/JqueryExercise/js/custom.js
+++ Esercizi/JqueryExercise/js/custom.js
@@ -2,9 +2,26 @@ $(function() {
   $("button").button();
   var hideButton = $( "#hideButton" );
 
+  $(".social").button({
+      icons: {
+        primary: "ui-icon-link"
+      },});
+
+
   hideButton.click(function() {
   	$("#textToHide").toggleClass( "hidden", 300, "easeOutSine" );
-  	$(this).html('<span class="ui-button-text">Show Content</span>');
+  	var text = $("span",this).text();
+  	$("span",this).text( text == "Show Content" ? text = "Hide Content" : "Show Content");
+  });
+
+  $("#showButton").click(function(){
+  	var src = $("#imgurl").val();
+  	var img = document.createElement("img");
+  	console.log(src);
+  	img.setAttribute("src",src);
+  	img.setAttribute("width",$("#pic").width());
+  	img.setAttribute("height",$("#pic").height());
+  	$("#pic").html(img);
   });
 
 
