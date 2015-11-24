0e1bfd48bcacc5269ab07fb35b1f505964651343
dodaro <dodaro@mat.unical.it>
2015-11-24 19:14:36 +0100

    Added Hibernate exercise.

---
+++ Hibernate1/src/persistence/UserDAO.java
@@ -0,0 +1,19 @@
+package persistence;
+
+import java.util.List;
+
+public interface UserDAO {
+	public void create(User user);
+
+	public User get(Integer id);
+
+	public User get(String username);
+
+	public void update(User user);
+
+	public void delete(User user);
+
+	public int numberOfUsers();
+
+	public List<User> getUsers();
+}
